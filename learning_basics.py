a8f007bffdcd4e399bfdef1236f996abb0cb2f39
Nitin Kumar <nitinkumar6912@gmail.com>
2021-02-12 09:35:00 +0530

    first commit

---
+++ learning_basics.py
@@ -0,0 +1,34 @@
+# a = 5*6
+# print(a)
+#
+# addition = 5 + 6
+# print('Sum value: ' + str(addition))
+
+
+# Types
+# string, example name = 'Nitin'
+# int, example number = 10
+# float, example number = 10.1
+# double, example double_value = 15.6
+# char, example mychar = 'c'
+# boolean, example value = True
+# array, example arr = [1, 2, 3]
+# tuple, example arr = (1, 2, 3)
+# dict, example dict = {'key': 'value', 'key2': 'value2'}
+
+# school = "myschool"
+# dict = {'key': 'value', 'name': 'Tim', 'age': 15, school: "My school is great."}
+# print(dict)
+# print(dict['key'])
+# print(dict['age'])
+# print(dict[school])
+#
+# dict['age'] = 48
+# dict = {'age': 456}
+# print(dict['age'])
+
+arr = [1, 2, 3]
+arr.append(4)
+print(arr)
+
+
